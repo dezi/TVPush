e9fc85353e012c816bd6e6d3e9d08ec305d854fc
dezi <dezi@kappa-mm.de>
2018-02-27 09:11:58 +0100

    Check in.

--- app/src/main/cpp/meme.c
+++ app/src/main/cpp/meme.c
@@ -34,6 +34,20 @@ void strtrim(char *str)
     }
 }
 
+int strncmp(register const char *s1, register const char *s2, register size_t n)
+{
+    if (n == 0) return 0;
+
+    do
+    {
+        if (*s1 != *s2++) return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+        if (*s1++ == 0) break;
+
+    } while (--n != 0);
+
+    return 0;
+}
+
 void getDeviceInfo()
 {
     FILE *fd = NULL;
@@ -341,21 +355,6 @@ int main(int argc, char *argv[])
     puts("\n");
 
     responder();
-
-    return 0;
-}
-
-int strncmp(register const char *s1, register const char *s2, register size_t n)
-{
-
-    if (n == 0) return 0;
-
-    do
-    {
-        if (*s1 != *s2++) return (*(unsigned char *)s1 - *(unsigned char *)--s2);
-        if (*s1++ == 0) break;
-
-    } while (--n != 0);
 
     return 0;
 }
