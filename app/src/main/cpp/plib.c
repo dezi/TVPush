eec34463ef202dadf4c6468e32209a8f1f6f1a9a
dezi <dezi@kappa-mm.de>
2018-03-03 13:28:23 +0100

    Check in.

--- app/src/main/cpp/plib.c
+++ app/src/main/cpp/plib.c
@@ -55,51 +55,76 @@ void replacedat(char *data, long size, char *target, char *patchs)
             size_t reallen = strlen(data + inx);
             strcpy(suffix, data + inx + tarlen);
 
+            /*
             int bevor1 = *(data + inx - 2);
             int bevor2 = *(data + inx - 1);
             int after1 = *(data + inx + reallen);
             int after2 = *(data + inx + reallen + 1);
 
-            printf("fund aaa %d %d %s => %s %02x (%c) %02x -- %02x %02x (%c)\n", inx, (int) reallen, data + inx, suffix,
+            printf("fund aaa %08dd %4d %s => %s %02x (%c) %02x -- %02x %02x (%c)\n", inx, (int) reallen, data + inx, suffix,
                    bevor1, bevor1, bevor2, after1, after2, after2);
+            */
+
+            printf("fund %08d %4d %s\n", inx, (int) reallen, target);
 
             memset(data + inx, 0, reallen);
             strcpy(data + inx, patchs);
             strcat(data + inx, suffix);
 
+            /*
             bevor1 = *(data + inx - 2);
             bevor2 = *(data + inx - 1);
             after1 = *(data + inx + reallen);
             after2 = *(data + inx + reallen + 1);
 
-            printf("fund bbb %d %d %s => %s %02x (%c) %02x -- %02x %02x (%c)\n", inx, (int) reallen, data + inx, suffix,
+            printf("fund bbb %08dd %4d %s => %s %02x (%c) %02x -- %02x %02x (%c)\n", inx, (int) reallen, data + inx, suffix,
                    bevor1, bevor1, bevor2, after1, after2, after2);
+            */
         }
     }
 }
 
 void zz_top_p2p_api()
 {
+    char* inpLib = "libPPPP_API.so";
+    char* outLib = "libzztopp2p_1.so";
+
+    char inpPath[ 256 ];
+
+    strcpy(inpPath, "/Users/dezi/TVPush/gen/");
+    strcat(inpPath, inpLib);
+
+    char outPath[ 256 ];
+    strcpy(outPath, "/Users/dezi/TVPush/app/src/main/jnilibs/armeabi-v7a/");
+    strcat(outPath, outLib);
+
+    char cpyPath[ 256 ];
+    strcpy(cpyPath, "/Users/dezi/TVPush/gen/");
+    strcat(cpyPath, outLib);
+
+    char strings[ 256 ];
+    strcpy(strings, "strings");
+    strcat(strings, " ");
+    strcat(strings, cpyPath);
+    strcat(strings, " > ");
+    strcat(strings, cpyPath);
+    strcat(strings, ".txt");
+
     long size;
-    char *data = readlib("/Users/dezi/TVPush/gen/libPPPP_API.so", &size);
-
-    //replacedat(data, size,
-    //           "Java_com_p2p_pppp_1api_PPPP_1APIs_PPPP_1",
-    //           "Java_zz_top_p2p_api_P2PApiNativexxxxxxx_");
+    char *data = readlib(inpPath, &size);
 
     replacedat(data, size,
-               "Java_com_p2p_pppp_1api_PPPP_1APIs_PPPP_1GetAPIVersion",
-               "Java_com_p2p_pppp_1api_PPPP_1APIs_PPPP_1GetAPIVersion");
+               "Java_com_p2p_pppp_1api_PPPP_1APIs_PPPP_1",
+               "Java_zz_top_p2p_api_P2PApiNative");
 
     replacedat(data, size,
-               "PPPP_GetAPIVersion",
-               "PPPP_GetAPIVersion");
+               "PPPP_",
+               "PPPP_");
 
-    writelib("/Users/dezi/TVPush/app/src/main/jnilibs/armeabi-v7a/libPPPP_API.so", data, size);
+    writelib(outPath, data, size);
+    writelib(cpyPath, data, size);
 
-    writelib("/Users/dezi/TVPush/gen/libzztopp2p.so", data, size);
-
-    system("strings /Users/dezi/TVPush/gen/libzztopp2p.so > /Users/dezi/TVPush/gen/libzztopp2p.txt");
+    system(strings);
 }
 
 int main(int argc, char *argv[])
