90bc2ec90ad74cf49e0a8fb754cfe3f625c58efa
dezi <dezi@kappa-mm.de>
2018-03-05 10:00:54 +0100

    iCheck in.

--- app/src/main/java/com/p2p/pppp_api/PPPP_Keys.java
+++
@@ -1,7 +0,0 @@
-package com.p2p.pppp_api;
-
-public class PPPP_Keys
-{
-    public static String licenseKey = "MYXXFG";
-    public static String serverString = "MNFFJLLHIFELKLHOOCEIPOEBGODBFPHGGKEALBIBIJBPCDBHPAIHABNCPPCFBFKDODKLDMPCNODIANDDBN";
-}
