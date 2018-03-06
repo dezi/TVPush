108ff5e7de39a6919cfe19bdbe37f81d9edd80d3
dezi <dezi@kappa-mm.de>
2018-03-06 09:59:33 +0100

    Check in.

--- zztop/src/main/java/zz/top/p2p/camera/P2PReaderThread.java
+++ zztop/src/main/java/zz/top/p2p/camera/P2PReaderThread.java
@@ -7,12 +7,12 @@ import zz.top.p2p.api.P2PApiNative;
 
 public class P2PReaderThread extends Thread
 {
-    private static final String LOGTAG = P2PReaderThread.class.getSimpleName();
+    public static final String LOGTAG = P2PReaderThread.class.getSimpleName();
 
     public final static byte CHANNEL_COMMAND = 0;
 
-    protected P2PSession session;
-    protected byte channel;
+    public P2PSession session;
+    public byte channel;
 
     public P2PReaderThread(P2PSession session, byte channel)
     {
@@ -34,13 +34,14 @@ public class P2PReaderThread extends Thread
             byte[] nBuffer = new byte[P2PHeader.HEADER_SIZE];
             int[] nSize = new int[]{nBuffer.length};
 
-            if (channel == CHANNEL_COMMAND) Log.d(LOGTAG, "head: wait channel=" + channel + " size=" + nSize[0]);
+            if (channel == CHANNEL_COMMAND)
+            {
+                Log.d(LOGTAG, "head: wait channel=" + channel + " size=" + nSize[0]);
+            }
 
             int resHead = P2PApiNative.Read(session.session, channel, nBuffer, nSize, -1);
 
-            if (! session.isConnected) break;
-
-            if (resHead == P2PApiErrors.ERROR_P2P_SESSION_CLOSED_CALLED)
+            if ((resHead == P2PApiErrors.ERROR_P2P_SESSION_CLOSED_CALLED) || !session.isConnected)
             {
                 Log.d(LOGTAG, "run: closed channel=" + this.channel);
 
@@ -55,66 +56,60 @@ public class P2PReaderThread extends Thread
 
                 break;
             }
-            else
+
+            P2PHeader header = P2PHeader.parse(nBuffer, 0, session.isBigEndian);
+
+            if (channel == CHANNEL_COMMAND)
             {
-                P2PHeader header = P2PHeader.parse(nBuffer,0, session.isBigEndian);
+                Log.d(LOGTAG, "head: read"
+                        + " channel=" + channel
+                        + " ioType=" + header.ioType
+                        + " version=" + header.version
+                        + " datasize=" + header.dataSize);
+            }
 
-                if (channel == CHANNEL_COMMAND)
-                {
-                    Log.d(LOGTAG, "head: read"
-                            + " channel=" + channel
-                            + " ioType=" + header.ioType
-                            + " version=" + header.version
-                            + " datasize=" + header.dataSize);
-                }
+            if ((header.dataSize < 0) || (header.dataSize > (2 * 1024 * 1024)))
+            {
+                Log.d(LOGTAG, "head: size corrupt...");
+                session.isCorrupted = true;
 
-                if ((header.dataSize < 0) || (header.dataSize > (2 * 1024 * 1024)))
-                {
-                    Log.d(LOGTAG, "head: size corrupt...");
-                    session.isCorrupted = true;
+                break;
+            }
 
-                    break;
-                }
-                else
-                {
-                    byte[] dBuffer =  new byte[ header.dataSize ];
-                    int[] dSize = new int[]{dBuffer.length};
+            byte[] dBuffer = new byte[header.dataSize];
+            int[] dSize = new int[]{dBuffer.length};
 
-                    if (channel == CHANNEL_COMMAND) Log.d(LOGTAG, "data: wait channel=" + channel + " size=" + dSize[0]);
+            if (channel == CHANNEL_COMMAND)
+            {
+                Log.d(LOGTAG, "data: wait channel=" + channel + " size=" + dSize[0]);
+            }
 
-                    int resData = P2PApiNative.Read(session.session, channel, dBuffer, dSize, -1);
+            int resData = P2PApiNative.Read(session.session, channel, dBuffer, dSize, -1);
 
-                    if (! session.isConnected) break;
+            if ((resData == P2PApiErrors.ERROR_P2P_SESSION_CLOSED_CALLED) || !session.isConnected)
+            {
+                Log.d(LOGTAG, "run: closed channel=" + this.channel);
 
-                    if (resData == P2PApiErrors.ERROR_P2P_SESSION_CLOSED_CALLED)
-                    {
-                        Log.d(LOGTAG, "run: closed channel=" + this.channel);
+                break;
+            }
 
-                        break;
-                    }
+            if ((resData != 0) || (dSize[0] != dBuffer.length))
+            {
+                Log.d(LOGTAG, "data: read corrupt channel=" + this.channel);
 
-                    if ((resData != 0) || (dSize[0] != dBuffer.length))
-                    {
-                        Log.d(LOGTAG, "data: read corrupt channel=" + this.channel);
-                        session.isCorrupted = true;
+                session.isCorrupted = true;
+                break;
+            }
 
-                        break;
-                    }
-                    else
-                    {
-                        if (channel == CHANNEL_COMMAND)
-                        {
-                            Log.d(LOGTAG, "data: read channel=" + channel + " res=" + resData + " size=" + dSize[0]);
-                        }
+            if (channel == CHANNEL_COMMAND)
+            {
+                Log.d(LOGTAG, "data: read channel=" + channel + " res=" + resData + " size=" + dSize[0]);
+            }
 
-                        if (! handleData(dBuffer, dSize[0]))
-                        {
-                            session.isCorrupted = true;
-
-                            break;
-                        }
-                    }
-                }
+            if (!handleData(dBuffer, dSize[0]))
+            {
+                session.isCorrupted = true;
+                break;
             }
         }
 
