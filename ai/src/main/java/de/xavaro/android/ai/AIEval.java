6d972c05c297eb39ea725e5a6192ece1f7f1010b
dezi <dezi@kappa-mm.de>
2018-03-09 16:06:25 +0100

    Check in.

---
+++ ai/src/main/java/de/xavaro/android/ai/AIEval.java
@@ -0,0 +1,15 @@
+package de.xavaro.android.ai;
+
+import org.json.JSONObject;
+
+public class AIEval
+{
+    public static JSONObject evaluate(String message)
+    {
+        JSONObject result = new JSONObject();
+
+
+
+        return result;
+    }
+}
