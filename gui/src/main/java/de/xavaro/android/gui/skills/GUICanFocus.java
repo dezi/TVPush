2968b01b3eba0228c8f59dcb2e2b0c12ad21eb66
dezi <dezi@kappa-mm.de>
2018-03-18 14:31:45 +0100

    Check in.

--- gui/src/main/java/de/xavaro/android/gui/skills/GUICanFocus.java
+++ gui/src/main/java/de/xavaro/android/gui/skills/GUICanFocus.java
@@ -5,6 +5,7 @@ import android.view.View;
 public interface GUICanFocus extends GUICanRoundedCorners
 {
     void setFocusable(boolean focusable);
+    void setActivatable(boolean activatable);
 
     int getBackgroundColor();
 
