55affafce3adba1fb73fdfa304bf0aa149cb268e
patrickz98 <patrickz98@users.noreply.github.com>
2018-03-28 20:06:18 +0200

    Check in.

--- gui/src/main/java/de/xavaro/android/gui/wizzards/GUIChannelWizzard.java
+++ gui/src/main/java/de/xavaro/android/gui/wizzards/GUIChannelWizzard.java
@@ -7,13 +7,16 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
+import de.xavaro.android.gui.views.GUILinearLayout;
 import de.xavaro.android.iot.base.IOT;
 
 import de.xavaro.android.gui.R;
@@ -30,6 +33,8 @@ import de.xavaro.android.iot.status.IOTMetadata;
 public class GUIChannelWizzard extends GUIPluginTitle
 {
     private final static String LOGTAG = GUIChannelWizzard.class.getSimpleName();
+
+    private int MENU_WIDTH = 150;
 
     private int CHANNEL_COLS = 5;
     private int CHANNEL_WIDTH;
@@ -52,19 +57,114 @@ public class GUIChannelWizzard extends GUIPluginTitle
         setTitleIcon(R.drawable.magic_hand_440);
         setTitleText("Channel Wizzard");
 
+//        contentFrame.setBackgroundColor(Color.BLUE);
+        contentFrame.setBackgroundColor(Color.WHITE);
+        GUILinearLayout menuContainer = createMenu();
+        contentFrame.addView(menuContainer);
+
+        GUIFrameLayout scrollDeFuck = new GUIFrameLayout(getContext());
+//        scrollDeFuck.setBackgroundColor(Color.RED);
+        menuContainer.addView(scrollDeFuck);
+
         scrollView = new GUIScrollView(getContext());
-        contentFrame.addView(scrollView);
+        scrollDeFuck.addView(scrollView);
 
         scrollContent = new GUIFrameLayout(getContext());
-        scrollContent.setBackgroundColor(Color.WHITE);
+//        scrollContent.setBackgroundColor(Color.WHITE);
         scrollView.addView(scrollContent);
 
         Log.d(LOGTAG, "GUIChannelWizzard: width=" + getPluginWidthDip() + " height=" + getPluginHeightDip());
         Log.d(LOGTAG, "GUIChannelWizzard: width=" + getPluginWidth() + " height=" + getPluginHeight());
         Log.d(LOGTAG, "GUIChannelWizzard: width=" + getPluginNettoWidth() + " height=" + getPluginNettoHeight());
 
-        CHANNEL_WIDTH = (getPluginNettoWidth() - scrollView.getScrollBarSize()) / CHANNEL_COLS;
+        CHANNEL_WIDTH = (getPluginNettoWidth() - scrollView.getScrollBarSize() - Simple.dipToPx(MENU_WIDTH)) / CHANNEL_COLS;
         CHANNEL_HEIGHT = Simple.dipToPx(40);
+    }
+
+    // ship with containers
+    private HashMap<GUIFrameLayout, GUITextView> buttonShip = new HashMap<>();
+
+    private GUIFrameLayout createMenuButton(String title, boolean addToShip)
+    {
+        GUIFrameLayout container = new GUIFrameLayout(getContext());
+        container.setMarginLeftDip(5);
+        container.setMarginRightDip(5);
+        container.setFocusable(true);
+        container.setSizeDip(Simple.MP, Simple.WC);
+
+        GUITextView button = new GUITextView(getContext());
+        button.setPaddingDip(5);
+        button.setText(title);
+        button.setTextColor(Color.WHITE);
+        button.setBackgroundColor(0xff5C6370);
+        button.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        button.saveBackground();
+        container.addView(button);
+
+        if (addToShip)
+        {
+            buttonShip.put(container, button);
+        }
+
+        return container;
+    }
+
+    private final OnClickListener modeListener = new OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            for (Map.Entry<GUIFrameLayout, GUITextView> entry : buttonShip.entrySet())
+            {
+                if (entry.getKey() == view)
+                {
+                    entry.getValue().saveBackground();
+                    entry.getValue().setBackgroundColor(0xff818896);
+                }
+                else
+                {
+                    entry.getValue().restoreBackground();
+                }
+            }
+        }
+    };
+
+    private GUILinearLayout createMenu()
+    {
+        GUILinearLayout menuContainer = new GUILinearLayout(getContext());
+        menuContainer.setPaddingDip(0);
+//        menuContainer.setMarginRightDip(0);
+//        menuContainer.setBackgroundColor(Color.RED);
+        menuContainer.setOrientation(LinearLayout.HORIZONTAL);
+        menuContainer.setGravity(Gravity.END);
+
+        GUILinearLayout menu = new GUILinearLayout(getContext());
+        menu.setBackgroundColor(0xff282C34);
+        menu.setOrientation(LinearLayout.VERTICAL);
+        menu.setSizeDip(MENU_WIDTH, Simple.MP);
+        menu.setGravity(Gravity.CENTER);
+        menuContainer.addView(menu);
+
+        GUIFrameLayout tvButton = createMenuButton("Tv", true);
+        menu.addView(tvButton);
+        tvButton.setOnClickListener(modeListener);
+
+        GUIFrameLayout radioButton = createMenuButton("Radio", true);
+        menu.addView(radioButton);
+        radioButton.setOnClickListener(modeListener);
+
+//        GUIFrameLayout allButton = createMenuButton("All", true);
+//        menu.addView(allButton);
+//        allButton.setOnClickListener(modeListener);
+//        allButton.callOnClick();
+
+        tvButton.callOnClick();
+
+        GUIFrameLayout saveButton = createMenuButton("Save", false);
+        saveButton.setMarginTopDip(20);
+        menu.addView(saveButton);
+
+        return menuContainer;
     }
 
     @Override
