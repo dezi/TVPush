bd7ac73e48424402bca5263ce5238be30b3de4fc
dezi <dezi@kappa-mm.de>
2018-03-28 14:06:42 +0200

    Check in.

---
+++ gui/src/main/java/de/xavaro/android/gui/plugin/GUIPluginTitle.java
@@ -0,0 +1,131 @@
+package de.xavaro.android.gui.plugin;
+
+import android.widget.LinearLayout;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.util.Log;
+
+import de.xavaro.android.gui.views.GUIIconViewIOT;
+import de.xavaro.android.gui.views.GUILinearLayout;
+import de.xavaro.android.gui.views.GUIFrameLayout;
+import de.xavaro.android.gui.views.GUIEditText;
+import de.xavaro.android.gui.views.GUITextView;
+
+import de.xavaro.android.gui.base.GUIDefs;
+
+import de.xavaro.android.gui.simple.Simple;
+
+public class GUIPluginTitle extends GUIPlugin
+{
+    private final static String LOGTAG = GUIPluginTitle.class.getSimpleName();
+
+    public GUIIconViewIOT titleIcon;
+    public GUITextView titleText;
+    public GUIEditText titleEdit;
+
+    private int titleIconResid;
+
+    public GUIPluginTitle(Context context)
+    {
+        super(context);
+
+        this.setPaddingDip(GUIDefs.PADDING_SMALL, GUIDefs.PADDING_ZERO, GUIDefs.PADDING_SMALL, GUIDefs.PADDING_SMALL);
+
+        GUILinearLayout splitterFrame = new GUILinearLayout(context);
+        splitterFrame.setOrientation(LinearLayout.VERTICAL);
+
+        contentFrame.addView(splitterFrame);
+
+        GUILinearLayout titleFrame = new GUILinearLayout(context);
+        titleFrame.setOrientation(LinearLayout.HORIZONTAL);
+        titleFrame.setSizeDip(Simple.MP, Simple.WC);
+
+        splitterFrame.addView(titleFrame);
+
+        titleIcon = new GUIIconViewIOT(context);
+        titleFrame.addView(titleIcon);
+
+        GUILinearLayout titleCenter = new GUILinearLayout(context);
+        titleCenter.setOrientation(LinearLayout.HORIZONTAL);
+        titleCenter.setGravity(Gravity.START + Gravity.CENTER_VERTICAL);
+        titleCenter.setSizeDip(Simple.MP, Simple.MP);
+
+        titleFrame.addView(titleCenter);
+
+        titleText = new GUITextView(context);
+        titleText.setPaddingDip(GUIDefs.PADDING_SMALL);
+        titleText.setTextSizeDip(GUIDefs.FONTSIZE_HEADERS);
+
+        titleCenter.addView(titleText);
+
+        titleEdit = new GUIEditText(context)
+        {
+            @Override
+            public void onHighlightChanged(View view, boolean highlight)
+            {
+                if (! highlight) onTitleEditFinished(view);
+            }
+        };
+
+        titleEdit.setSizeDip(Simple.MP, Simple.WC);
+        titleEdit.setPaddingDip(GUIDefs.PADDING_SMALL);
+        titleEdit.setTextSizeDip(GUIDefs.FONTSIZE_HEADERS);
+        titleEdit.setHighlightable(false);
+        titleEdit.setFocusable(false);
+        titleEdit.setVisibility(GONE);
+
+        titleCenter.addView(titleEdit);
+
+        contentFrame = new GUIFrameLayout(context);
+        contentFrame.setSizeDip(Simple.MP, Simple.MP);
+
+        splitterFrame.addView(contentFrame);
+    }
+
+    public void setTitleIcon(int resid)
+    {
+        titleIconResid = resid;
+
+        titleIcon.setImageResource(resid);
+    }
+
+    public int getTitleIconResid()
+    {
+        return titleIconResid;
+    }
+
+    public void setTitleText(String text)
+    {
+        titleText.setText(text);
+    }
+
+    public String getTitleText()
+    {
+        return titleText.getText().toString();
+    }
+
+    public void setTitleEdit(String text, String hint, String toast)
+    {
+        if ((text != null) && text.equals(titleText.getText().toString()))
+        {
+            //
+            // Unset bogus self repeating stuff.
+            //
+
+            text = "";
+        }
+
+        titleEdit.setText(text);
+        titleEdit.setHint(hint);
+        titleEdit.setFocusable(true);
+        titleEdit.setToastFocus(toast);
+        titleEdit.setHighlightable(true);
+        titleEdit.setVisibility(VISIBLE);
+    }
+
+    public void onTitleEditFinished(View view)
+    {
+        Log.d(LOGTAG, "onTitleEditFinished: STUB!");
+    }
+}
---
+++ gui/src/main/java/de/xavaro/android/gui/plugin/GUIPluginTitleList.java
@@ -0,0 +1,114 @@
+package de.xavaro.android.gui.plugin;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.view.View;
+
+import de.xavaro.android.gui.base.GUIDefs;
+import de.xavaro.android.gui.simple.Simple;
+import de.xavaro.android.gui.views.GUIListView;
+import de.xavaro.android.gui.views.GUIScrollView;
+
+public class GUIPluginTitleList extends GUIPluginTitle
+{
+    private final static String LOGTAG = GUIPluginTitleList.class.getSimpleName();
+
+    public GUIScrollView scrollView;
+    public GUIListView listView;
+
+    public GUIPluginTitleList(Context context)
+    {
+        super(context);
+
+        scrollView = new GUIScrollView(context);
+        scrollView.setRoundedCorners(GUIDefs.ROUNDED_MEDIUM,GUIDefs.COLOR_LIGHT_TRANSPARENT);
+        scrollView.setPaddingDip(GUIDefs.PADDING_SMALL);
+
+        contentFrame.addView(scrollView);
+
+        listView = new GUIListView(context);
+
+        scrollView.addView(listView);
+    }
+
+    @Override
+    public void onAttachedToWindow()
+    {
+        super.onAttachedToWindow();
+
+        Simple.getHandler().post(makeEntryList);
+    }
+
+    @Override
+    public void onDetachedFromWindow()
+    {
+        super.onDetachedFromWindow();
+
+        Simple.getHandler().removeCallbacks(makeEntryList);
+
+        listView.removeAllViews();
+    }
+
+    public void onCollectEntries(GUIListView listView, boolean todo)
+    {
+        Log.d(LOGTAG, "onCollectEntries: Stub!");
+    }
+
+    public void updateContent()
+    {
+        Simple.getHandler().removeCallbacks(makeEntryList);
+        Simple.getHandler().post(makeEntryList);
+    }
+
+    private final Runnable makeEntryList = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            listView.markAllViewsUnused();
+
+            onCollectEntries(listView, false);
+
+            listView.removeAllUnusedViews();
+
+            Simple.getHandler().postDelayed(makeEntryList, 10 * 1000);
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        if ((requestCode == 4711) && (permissions.length > 0) && (grantResults.length > 0))
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Log.d(LOGTAG, "onRequestPermissionsResult: yep=" + permissions[ 0 ]);
+
+                Simple.getHandler().removeCallbacks(makeEntryList);
+                Simple.getHandler().post(makeEntryList);
+            }
+            else
+            {
+                Log.d(LOGTAG, "onRequestPermissionsResult: boo=" + permissions[ 0 ]);
+            }
+        }
+    }
+
+    public static void updateContentinParentPlugin(View view)
+    {
+        while (! (view instanceof GUIPluginTitleList))
+        {
+            if (view.getParent() instanceof View)
+            {
+                view = (View) view.getParent();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        ((GUIPluginTitleList) view).updateContent();
+    }
+}
