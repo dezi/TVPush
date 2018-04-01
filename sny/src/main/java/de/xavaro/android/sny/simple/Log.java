8ec5b0e40539306021ff2cc0aa80782b559bd03c
dezi <dezi@kappa-mm.de>
2018-04-01 08:33:18 +0200

    Check in.

---
+++ sny/src/main/java/de/xavaro/android/sny/simple/Log.java
@@ -0,0 +1,48 @@
+package de.xavaro.android.sny.simple;
+
+public class Log
+{
+    public static boolean debug = false;
+
+    public static String[] allow = new String[]
+            {
+                    "GLSRenderer",
+                    "p2p*",
+                    "sny*"
+            };
+
+    public static void d(String logtag, String message)
+    {
+        if (debug || checkLog(allow, logtag))
+        {
+            android.util.Log.d(logtag, message);
+        }
+    }
+
+    public static void e(String logtag, String message)
+    {
+        android.util.Log.e(logtag, message);
+    }
+
+    public static boolean checkLog(String[] checks, String logtag)
+    {
+        logtag = logtag.toLowerCase();
+
+        for (String check : checks)
+        {
+            check = check.toLowerCase();
+
+            if (check.endsWith("*") && logtag.startsWith(check.substring(0, check.length() - 2)))
+            {
+                return true;
+            }
+
+            if (check.equalsIgnoreCase(logtag))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ sny/src/main/java/de/xavaro/android/sny/simple/Simple.java
@@ -0,0 +1,110 @@
+package de.xavaro.android.sny.simple;
+
+import android.support.annotation.Nullable;
+
+import android.content.ContentResolver;
+import android.provider.Settings;
+import android.os.Handler;
+import android.os.Build;
+
+import android.app.Application;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.text.format.Formatter;
+
+import java.nio.ByteBuffer;
+import java.security.Key;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Simple
+{
+    private static Handler handler;
+    private static WifiManager wifiManager;
+    private static ContentResolver contentResolver;
+
+    public static void initialize(Application app)
+    {
+        handler = new Handler();
+        wifiManager = (WifiManager) app.getSystemService(Context.WIFI_SERVICE);
+        contentResolver = app.getContentResolver();
+    }
+
+    @Nullable
+    public static String hmacSha1UUID(String key, String data)
+    {
+        try
+        {
+            Key secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA1");
+            Mac instance = Mac.getInstance("HmacSHA1");
+            instance.init(secretKeySpec);
+
+            byte[] bytes = instance.doFinal(data.getBytes("UTF-8"));
+
+            ByteBuffer bb = ByteBuffer.wrap(bytes,0 , 16);
+
+            long high = bb.getLong();
+            long low = bb.getLong();
+
+            UUID uuid = new UUID(high, low);
+
+            return uuid.toString();
+        }
+        catch (Exception ignore)
+        {
+        }
+
+        return null;
+    }
+
+    public static Handler getHandler()
+    {
+        return handler;
+    }
+
+    @Nullable
+    public static String getConnectedWifiName()
+    {
+        if (wifiManager == null) return null;
+
+        String wifi = wifiManager.getConnectionInfo().getSSID();
+        return wifi.replace("\"", "");
+    }
+
+    @Nullable
+    @SuppressWarnings("deprecation")
+    public static String getConnectedWifiIPAddress()
+    {
+        if (wifiManager == null) return null;
+
+        int ipint = wifiManager.getConnectionInfo().getIpAddress();
+        return Formatter.formatIpAddress(ipint);
+    }
+
+    public static String getDeviceUserName()
+    {
+        return Settings.Secure.getString(contentResolver, "bluetooth_name");
+    }
+
+    public static String getMapString(Map<String, String> map, String key)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            return map.getOrDefault(key, null);
+        }
+        else
+        {
+            try
+            {
+                return map.get(key);
+            }
+            catch (Exception ignore)
+            {
+                return null;
+            }
+        }
+    }
+}
